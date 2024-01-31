75fc1a10fac924e6bbeeb9abd9b4ba5851a51d82
Colm Murphy <colmmurphy016@gmail.com>
2024-01-31 14:33:26 +0000

    add functionality to pan camera with arrow keys

--- PlayerCamera.gd
+++ PlayerCamera.gd
@@ -1,5 +1,6 @@
 extends Camera2D
 
+@export var camera_pan_speed: int = 400
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -8,4 +9,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var velocity: Vector2 = Vector2.ZERO
+	if Input.is_action_pressed("camera_pan_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("camera_pan_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("camera_pan_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("camera_pan_up"):
+		velocity.y -= 1
+		
+	position += velocity * camera_pan_speed * delta
