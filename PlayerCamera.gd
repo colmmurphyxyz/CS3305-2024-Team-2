9d74dfd38471512f11d683a5aac8e96ecc6319de
Colm Murphy <colmmurphy016@gmail.com>
2024-02-01 10:43:46 +0000

    resolve jittering issue

--- PlayerCamera.gd
+++ PlayerCamera.gd
@@ -4,6 +4,8 @@ extends Camera2D
 @export var camera_zoom: float = 0.1
 @export var camera_zoom_min: float = 0.3
 @export var camera_zoom_max: float = 2.5
+# multiplier for the speed the camera moves at when panning with the middle mouse button
+@export var camera_mmb_pan_multiplier: float = 0.02
 @onready var camera_zoom_delta = Vector2(camera_zoom, camera_zoom)
 
 # Called when the node enters the scene tree for the first time.
@@ -14,6 +16,7 @@ var mmb_initial_pos: Vector2 = Vector2.ZERO
 var mmb_pressed_initial_camera_pos: Vector2 = Vector2.ZERO
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float):
+	print("DEBUG: camera position: ", position, ", global position: ", global_position)
 	var velocity: Vector2 = Vector2.ZERO
 	# pan camera with arrow keys
 	if Input.is_action_pressed("camera_pan_left"):
@@ -40,10 +43,10 @@ func _process(delta: float):
 	# mmb camera pan
 	if Input.is_action_just_pressed("camera_pan_mmb"):
 		mmb_initial_pos = get_global_mouse_position()
-		mmb_pressed_initial_camera_pos = global_position
+		mmb_pressed_initial_camera_pos = position
 	if Input.is_action_pressed("camera_pan_mmb"):
 		var mmb_distance: Vector2 = get_global_mouse_position() - mmb_initial_pos
-		global_position = mmb_pressed_initial_camera_pos - mmb_distance
+		velocity += -camera_mmb_pan_multiplier * mmb_distance
 	
 		
 	# pan camera if mouse at camera bounds
@@ -51,4 +54,5 @@ func _process(delta: float):
 	var camera_center: Vector2 = get_screen_center_position()
 	# if cursor's distance from center is > 0.9 * distance from center to bounds
 	# pan the camera in that direction
-	position += velocity * camera_pan_speed * delta
+	if velocity.length() > 0:
+		position += velocity * camera_pan_speed * delta
