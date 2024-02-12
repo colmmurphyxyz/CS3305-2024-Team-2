b3cd0587ed4dbde308d9a50238b56db0484acd00
Colm Murphy <colmmurphy016@gmail.com>
2024-02-12 18:32:05 +0000

    remove base units from multiplayer test scene

--- Camera/PlayerCamera.gd
+++ Camera/PlayerCamera.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
+## Enable/disable camera pan with the mouse cursor.
+## Can be useful when running multiple instances of the game.
+## Rr some players may just prefer it disabled.
 @export var enable_cursor_pan: bool = true
+## Can also be toggled with the z key
 @export var is_locked: bool = false
 
-var camera_size: Vector2 = get_viewport_rect().size
+var camera_size: Vector2
 
 func _ready():
-	pass
+	camera_size = get_viewport_rect().size
 	
 func _process(delta: float):
 	camera_size = get_viewport_rect().size / $Camera2D.zoom
