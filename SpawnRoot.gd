eac085992e664f09a654ae639193e7801fbea0b7
Colm Murphy <colmmurphy016@gmail.com>
2024-02-18 17:17:41 +0000

    scout netcode

--- SpawnRoot.gd
+++ SpawnRoot.gd
@@ -19,6 +19,10 @@ func _input(event: InputEvent):
 				spawn_unit.rpc_id(1, multiplayer.get_unique_id(), \
 						"res://Unit/UnitTypes/Warden/warden.tscn", \
 						get_global_mouse_position())
+			KEY_T:
+				spawn_unit.rpc_id(1, multiplayer.get_unique_id(), \
+						"res://Unit/UnitTypes/Scout/scout.tscn", \
+						get_global_mouse_position())
 				
 @rpc("any_peer", "call_local", "reliable")
 func spawn_unit(called_by: int, scene_path: String, spawn_pos: Vector2):
