29b8bcf8a8ce6560232d67d26df5dbc5d104a9fe
David Wilson <szabol.david04@gmail.com>
2024-02-21 13:26:30 +0000

    HQ Fixes
    Fixed repairing bug
    Moved hq interaction into new scene

--- Buildings/HQ/hq.gd
+++ Buildings/HQ/hq.gd
@@ -5,69 +5,29 @@ var context_menu_instance
 
 var free_spawn = 60.0
 
+var control = null
+
 func _ready():
 	super._ready()
 	max_hp = 100.0
 	health = max_hp / 2
 	is_active = false
-	remove_from_group("Constructions")
+	control = get_node("Control")
+	control.hide()
 	
 func _process(delta):
 	super._process(delta)
 	if free_spawn > 0:
 		free_spawn -= delta
-	#if health <= 0:
-		##loss senario
-		#pass
-	#if health < max_hp:
-		#if in_area.size() > 0:
-			#health += delta * in_area.size()
-			#if health >= max_hp:
-				#is_active = true
-				##print("Repair complete!")
-			##print("Repairing...", health, "/", max_hp)
-		#else:
-			##print("Repair stopped")
-			#pass
 
 
 func _input(event):
 	if event.is_action_pressed("right_click"):
 		if get_global_mouse_position().distance_to(global_transform.origin) < 50:
-			on_right_click()
+			control.show()
+		else:
+			control.hide()
 	pass
-
-func on_right_click():
-	show_context_menu()
-
-func show_context_menu():
-	# Check if the context menu is not already visible
-	if not context_menu_instance:
-		# Instantiate the context menu scene
-		context_menu_instance = context_menu_scene.instantiate()
-		
-		# Set the position of the context menu next to the StaticBody2D
-		var position = get_viewport().get_camera_2d().get_global_mouse_position()
-		
-		position.x += 10  # Offset to the right
-		context_menu_instance.global_position = position
-		
-		# Add the context menu as a child of the current scene
-		add_child(context_menu_instance)
-		
-		# Connect the context menu buttons to their respective functions
-		connect_context_menu_buttons()
-		context_menu_instance.scale = Vector2(0.5, 0.5)
-		# Connect a signal to hide the context menu when a button is pressed
-		if context_menu_instance.has_node("Spawn"):  # Replace with the actual name of your button node
-			var button = context_menu_instance.get_node("Spawn")
-			button.pressed.connect(_on_context_menu_button_pressed)
-
-func connect_context_menu_buttons():
-	# Connect each button's "pressed" signal to a function
-	for button in context_menu_instance.get_children():
-		if button is Button:
-			button.presses.connect(_on_context_menu_button_pressed)
 
 func _on_context_menu_button_pressed(button_text):
 	# Handle the button press
